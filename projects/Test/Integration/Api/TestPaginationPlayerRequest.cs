2831e8c54c9a8f8bea137e5e330b4515c87a81f8
Josue Placido <josueplacidodasilveirajunior@hotmail.com>
2021-01-23 16:16:57 -0300

    test(backend): test para paginação de jogadores
    AB#388
    
    - Criação dos teste na camada de aplicação
    - Testes dos request na API

---
+++ projects/Test/Integration/Api/TestPaginationPlayerRequest.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Keeper.Api;
+using Keeper.Application.Interface;
+using Keeper.Application.Models;
+using Keeper.Domain.Models;
+using Keeper.Infrastructure.CrossCutting.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Test.DataExamples;
+using Test.Utils;
+using Xunit;
+
+namespace Keeper.Test.Integration.Api
+{
+	[Collection("WebApi Collection")]
+	public class TestPaginationPlayerRequest : IClassFixture<WebApplicationFactory<Startup>>
+	{
+		private readonly CustomWebApplicationFactoryFixture _fixture;
+		public TestPaginationPlayerRequest(CustomWebApplicationFactoryFixture fixture) => this._fixture = fixture;
+
+		[Fact]
+		public async Task Get_PlayersList_ReturnPlayerList()
+		{
+
+			HttpClient client = this._fixture
+				.CustomWebApplicationFactory
+				.CreateClient();
+
+			HttpResponseMessage actualResponse = await client
+				.GetAsync("/Player?terms=test")
+				.ConfigureAwait(false);
+
+			var result = JsonConvert.DeserializeObject<PlayerPaginationDTO>(
+				await actualResponse.Content.ReadAsStringAsync());
+			actualResponse.EnsureSuccessStatusCode();
+			Assert.Equal(HttpStatusCode.OK, actualResponse.StatusCode);
+			Assert.True(result.Page == 1);
+			Assert.True(result.Take == 10);
+			Assert.True(result.Terms == "test");
+			Assert.True(string.IsNullOrEmpty(result.ExcludeFromChampionship));
+			Assert.IsType<Player[]>(result.Players);
+		}
+	}
+}
---
+++ projects/Test/UnitTest/Application/Service/PlayerServiceList.cs
@@ -0,0 +1,84 @@
+
+using System.Linq;
+using Keeper.Application.Services;
+using AutoMapper;
+using Xunit;
+using Xunit.Abstractions;
+using Keeper.Infrastructure.Repository;
+using Keeper.Infrastructure.DAO;
+using System.Collections.Generic;
+using Keeper.Domain.Models;
+using Keeper.Infrastructure.CrossCutting.DTO;
+
+namespace Keeper.Test.UnitTest.Application.Service
+{
+	public class PlayerServiceList : IClassFixture<SharedDatabaseFixture>
+	{
+		private readonly ITestOutputHelper _output;
+		public SharedDatabaseFixture Fixture { get; }
+		public PlayerServiceList(SharedDatabaseFixture fixture, ITestOutputHelper output)
+			=> (_output, Fixture) = (output, fixture);
+
+		[Fact]
+		public void Get_PlayerList_PageAndTakes()
+		{
+			var expected = SeedData.Players;
+			int pages = expected.Length / 5;
+			List<Player> finalList = new List<Player>();
+			using (var context = Fixture.CreateContext())
+			{
+				PlayerRepository repo = new PlayerRepository(context);
+				DAOPlayer dao = new DAOPlayer(context);
+				var service = new PlayerService(null, repo, dao);
+				PlayerPaginationDTO result = null;
+				for (int p = 1; p <= pages; p++)
+				{
+					result = service.GetAvailables(page: p, take: 5).Result;
+					Assert.True(result.Total == expected.Length);
+					Assert.True(result.Page == p);
+					Assert.True(result.Players.Length == 5);
+					finalList.AddRange(result.Players);
+				}
+				result = service.GetAvailables(page: 3, take: 5).Result;
+				Assert.True(result.Total == expected.Length);
+				Assert.True(result.Page == 3);
+				Assert.True(result.Players.Length == 1);
+				finalList.AddRange(result.Players);
+				Assert.True(expected.Length == finalList.Count);
+				Assert.All(result.Players, item => expected.Contains(item));
+				Assert.All(result.Players, item => expected.Contains(item));
+			}
+		}
+		[Fact]
+		public void Get_PlayerList_WithTerms()
+		{
+			using (var context = Fixture.CreateContext())
+			{
+				PlayerRepository repo = new PlayerRepository(context);
+				var result = new PlayerService(null, repo, new DAOPlayer(context))
+					.GetAvailables("player").Result;
+				var expected = SeedData.Players.Take(4);
+				Assert.True(result.Total == 4);
+				Assert.True(result.Terms == "player");
+				Assert.True(result.Players.Length == expected.Count());
+				Assert.All(result.Players, item => expected.Contains(item));
+			}
+		}
+		[Fact]
+		public void Get_PlayerList_NotInChampionship()
+		{
+			using (var context = Fixture.CreateContext())
+			{
+				string championship = new ChampionshipRepository(context).GetAll().Result.LastOrDefault().Id;
+				PlayerRepository repo = new PlayerRepository(context);
+				var result = new PlayerService(null, repo, new DAOPlayer(context))
+					.GetAvailables(championship: championship).Result;
+				var expected = SeedData.Players;
+				Assert.True(result.Total == 5);
+				Assert.True(result.ExcludeFromChampionship == championship);
+				Assert.True(result.Players.Length == 5);
+				Assert.All(result.Players, item => expected.Contains(item));
+			}
+		}
+	}
+}
